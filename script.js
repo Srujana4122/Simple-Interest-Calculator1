5acc85bca163894c0293a9bf1d2ce1b66d0bd105
Srujana4122 <83061533+Srujana4122@users.noreply.github.com>
2021-07-27 13:27:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    t = document.getElementById("years").value;
+    r = document.getElementById("rate").value;
+    result = (p*t*r)/100;
     
 }
         
