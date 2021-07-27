c8e3faf58086189377b22e56804dda0c5d412600
Srujana4122 <83061533+Srujana4122@users.noreply.github.com>
2021-07-27 13:41:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ function compute()
     t = document.getElementById("years").value;
     r = document.getElementById("rate").value;
     result = (p*t*r)/100;
+    document.write("If you deposit %d",p);
+    document.write("<br>"at an interest rate of %d%",r</br>");
+    document.write("<br>"You will receive an amount of %d",result</br>");
+    document.write("in the year %d",+new Date().getFullYear()+t);
     
 }
         
